91a79dd1328eb9a6cef63ae2da7c794bc298c23e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-01 14:30:36 +0900

    Add support reference-style links
    E.g. `[#123][]` or `[@user1][]`

--- src/__tests__/changelogToGithubRelease.test.js
+++ src/__tests__/changelogToGithubRelease.test.js
@@ -6,6 +6,9 @@ import { changelogToGithubRelease } from '../changelogToGithubRelease.js';
 const changelog = `
 # Changelog
 
+## 1.2.0
+- reference-style link [#456][] ([@user1]).
+
 ## 1.1.0 - 2024-12-31
 - ddd [#123](https://github.com/foo/bar/pull/123) ([@user](https://github.com/user)).
 - eee.
@@ -16,26 +19,24 @@ const changelog = `
 
 ## 0.1.0
 - ccc.
+
+[#456]: https://github.com/foo/bar/pull/123
+[@user1]: https://github.com/user1
 `;
+
+test('include reference links correctly', { only: true }, async () => {
+	const result = await changelogToGithubRelease(changelog, '1.2.0');
+	assert.equal(result, '* reference-style link #456 (@user1).\n');
+});
 
 test('rewrite change items for version with date in heading', async () => {
 	const result = await changelogToGithubRelease(changelog, '1.1.0');
-	assert.equal(
-		result,
-		`* ddd #123 (@user).
-* eee.
-`,
-	);
+	assert.equal(result, '* ddd #123 (@user).\n* eee.\n');
 });
 
 test('rewrite change items matching specified version', async () => {
 	const result = await changelogToGithubRelease(changelog, '1.0.0');
-	assert.equal(
-		result,
-		`* aaa #123 (@user).
-* bbb.
-`,
-	);
+	assert.equal(result, '* aaa #123 (@user).\n* bbb.\n');
 });
 
 test('raise an error when a specified version is not found', async () => {
