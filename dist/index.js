e9a8827329035e7af4382089d743f21e8f541214
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-10-01 13:27:24 +0900

    Improve error messages to help troubleshooting
    In addition, this improves the README and the `tag` input description as the documentation.

--- dist/index.js
+++ dist/index.js
@@ -50393,6 +50393,7 @@ const remark = unified().use(remarkParse).use(remarkStringify).freeze()
  */
 function extractChangeItems({ version }) {
 	return (tree) => {
+		let foundVersion = false;
 		/** @type {import('mdast').List | undefined} */
 		let list;
 
@@ -50404,6 +50405,7 @@ function extractChangeItems({ version }) {
 				parent &&
 				index !== undefined
 			) {
+				foundVersion = true;
 				const nextSiblings = parent.children.slice(index + 1);
 				for (const sibling of nextSiblings) {
 					if (sibling.type === 'list') {
@@ -50415,8 +50417,12 @@ function extractChangeItems({ version }) {
 			return CONTINUE;
 		});
 
+		if (!foundVersion) {
+			throw new Error(`Not found the version ${version} in the changelog`);
+		}
+
 		if (!list) {
-			throw new Error(`Not found version: ${version}`);
+			throw new Error(`Not found list under the ${version} heading in the changelog`);
 		}
 
 		// Rewrite a link or reference to a PR notation (#123) or a user mention (@username).
